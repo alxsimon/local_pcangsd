a337243b525d288ec61b8e9c351dcb88d995b944
Alexis <alexis.simon@normalesup.org>
2021-06-17 16:36:26 +0200

    drop lostruct for now

--- src/local_pcangsd/local_pcangsd.py
+++ src/local_pcangsd/local_pcangsd.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from pcangsd_core import shared
 from pcangsd_core import covariance
-import lostruct
+# import lostruct
 import multiprocessing as mp
 import os, contextlib
 
