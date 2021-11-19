07141724e300a42dd8bf79a4e65ef75df58ee2e4
Alexis <alexis.simon@normalesup.org>
2021-11-19 10:53:09 +0100

    include variant_contig variable and attribute contigs that will be useful with sgkit functions

--- src/local_pcangsd/local_pcangsd.py
+++ src/local_pcangsd/local_pcangsd.py
@@ -7,7 +7,11 @@ DIM_SAMPLE = "samples"
 DIM_ALLELE = "alleles"
 DIM_GENOTYPE = "genotypes"
 
-def create_dataset(df: pd.DataFrame) -> xr.Dataset:
+def create_dataset(df: pd.DataFrame, contigs: list) -> xr.Dataset:
+    """
+    df: pandas DataFrame (chunk of the beagle file)
+    contigs: list of contig names
+    """
     sample_id = np.array(df.columns[range(3, df.shape[1], 3)])
 
     variants = df.marker.str.rsplit('_', 1, expand=True)
@@ -24,24 +28,34 @@ def create_dataset(df: pd.DataFrame) -> xr.Dataset:
         axis=-1
     )
 
+    variant_contig = np.array([contigs.index(n) for n in variants.contig], dtype=np.int64)
+
     ds = xr.Dataset(
         data_vars={
             'variant_contig_name': ([DIM_VARIANT], variants.contig.to_numpy()),
+            'variant_contig': ([DIM_VARIANT], variant_contig),
             'variant_position': ([DIM_VARIANT], variants.position.to_numpy(dtype=np.int64)),
             'sample_id': ([DIM_SAMPLE], sample_id),
             'allele': ([DIM_VARIANT, DIM_ALLELE], df.iloc[:,[1,2]].to_numpy(dtype=np.int8)),
             'genotype_likelihood': ([DIM_VARIANT, DIM_SAMPLE, DIM_GENOTYPE], GL),
-        }
+        },
+        attrs=dict(contigs=contigs),
     )
     return ds
+
 
 def beagle_to_zarr(input: str, store: str, chunksize: int=10000) -> None:
     """
     Converts an ANGSD genotype likelihood dataset to a Zarr array on disk.
     """
+    # first pass to obtain contig names
+    df_chunked = pd.read_csv(input, sep='\t', chunksize=chunksize)
+    contigs = [df.marker.str.rsplit('_', 1, expand=True).iloc[:,0].unique() for df in df_chunked]
+    contigs = list(np.unique(np.hstack(contigs)))
+
     df_chunked = pd.read_csv(input, sep='\t', chunksize=chunksize)
     for i, df in enumerate(df_chunked):
-        ds = create_dataset(df)
+        ds = create_dataset(df, contigs=contigs)
         if i == 0:
             ds.to_zarr(store, mode="w")
         else:
@@ -54,6 +68,7 @@ def load_dataset(store: str, chunksize: int=10000) -> xr.Dataset:
 
 
 # function for windowing
+# Wrapper of sgkit windowing functions
 
 
 
