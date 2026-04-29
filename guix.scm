e5d94e445a1267343f3e76380fbc59baf7f350bf
Alexis Simon <alexis.simon@runbox.com>
2026-04-29 15:35:16 +0200

    finish the guix.scm package

--- guix.scm
+++ guix.scm
@@ -7,38 +7,8 @@
              (gnu packages python-build)
              (gnu packages python-xyz)
              (gnu packages python-science)
-             (guix-arg packages python-extra))
-             ; (guix-science packages bioinformatics))
-
-(define-public python-pcangsd
-  (package
-    (name "python-pcangsd")
-    (version "1.36.4")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/Rosemeis/pcangsd")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "04j5545z8lz9fjac9smx095m7zdjb6gli1nmsm28r4ha4pp17g9g"))))
-    (build-system pyproject-build-system)
-    (arguments
-      (list
-        ;; No tests in package.
-        #:tests? #f))
-    (propagated-inputs (list python-numpy
-                             python-scipy))
-    (native-inputs (list python-cython-next
-                         python-setuptools))
-    (home-page "https://github.com/Rosemeis/pcangsd")
-    (synopsis
-     "Framework for analyzing low depth @acronym{NGS, Next-Generation Sequencing} data using @acronym{PCA, Principal Component Analysis}")
-    (description
-     "Framework for analyzing low-depth @acronym{NGS, Next-Generation Sequencing} data
-in heterogeneous/structured populations using @acronym{PCA, Principal Component Analysis}.")
-    (license license:gpl3)))
+             (guix-arg packages python-extra)
+             (guix-science packages bioinformatics))
 
 (package
   (name "python-local-pcangsd")
